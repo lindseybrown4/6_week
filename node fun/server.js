9fc9293c22d065e5b301ac7c7b7a6e2de76e8c91
lindseybrown4 <lindseybrown4@gmail.com>
2015-08-10 14:15:50 -0600

    more changes mon

--- node fun/server.js
+++ node fun/server.js
@@ -1,16 +1,68 @@
 
 
-////////DEPENDENCIES////////////
+///////////////////////////////////////
+////////      DEPENDENCIES      ///////
+///////////////////////////////////////
 
 var express = require('express'); 
 var bodyParser = require('body-parser'); 
+var cors = require('cors'); 
+var request = require('request');
+var twilio = require('twilio'); 
+
+var accountSid = ''; 
+var authToken = ''; 
+ 
+var client = require('twilio')(accountSid, authToken); 
 
 var app = express(); 
 var port = 8080; 
 
+///////////////////////////////////////
+////////      MIDDLEWARE     //////////
+///////////////////////////////////////
+
+app.use(express.static('public'));   // giving the browser the public folder
+// app.use(bodyParser());  //-------normal
+app.use(bodyParser.urlencoded({
+	extended: true
+})); 
+
+app.use(bodyParser.json()); 
+app.use(cors()); 
+
+var message = {"message": "HELLO WORLD!!!!"}
+
+
+///////////////////////////////////////
+////////      ENDPOINTS    ////////////
+///////////////////////////////////////
+
+app.get('/api/message', function(req, res){
+	// return res.send({"message": "HELLO WORLD!!!!!"})
+	return res.json(message); 
+}); 
+
+app.post('/api/send_text_message', function(req, res) {
+	console.log(req.body.message); 
+	// request.post('https://' + accountSid + ':' + authToken + '@api.twilio.com/2010-0-01/Accounts/')
+
+
+		client.messages.create({ 
+		to: "8185220057", 
+		from: "+18183517988", 
+		body: "You Rock at this stuff",   
+	}, function(err, message) { 
+	   console.log(message.sid); 
+	});
+}); 
+
+
+
+
+
 app.listen(port, function() {
 	console.log("I am watching you on... " + port); 
+}); 
 
-}; 
-///////
 
